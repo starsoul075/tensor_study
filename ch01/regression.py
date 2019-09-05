0a5d49e494a545ea50c05fc1c38855ca5695d421
帆 文 <fan.wen@datia.cn>
2019-09-05 16:53:17 +0800

    ch1 finish

---
+++ ch01/regression.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+from tensorflow import keras
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+dataset_path = keras.utils.get_file('auto-mpg.data',
+                                    'http://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data')
+column_names = ['MPG', 'Cylinders', 'Displacement', 'Horsepower', 'Weight',
+                'Acceleration', 'Model Year', 'Origin']
+raw_dataset = pd.read_csv(dataset_path, names=column_names,
+                      na_values = "?", comment='\t',
+                      sep=" ", skipinitialspace=True)
+
+dataset = raw_dataset.copy()
+dataset = dataset.dropna()
+origin = dataset.pop('Origin')
+dataset['USA'] = (origin == 1)*1.0
+dataset['Europe'] = (origin == 2)*1.0
+dataset['Japan'] = (origin == 3)*1.0
+
+train_dataset = dataset.sample(frac=0.8, random_state=0)
+test_dataset = dataset.drop(train_dataset.index)
+
+sns.pairplot(train_dataset[["MPG", "Cylinders", "Displacement", "Weight"]], diag_kind="kde")
+train_stats = train_dataset.describe()
+train_stats.pop("MPG")
+train_stats = train_stats.transpose()
+
+train_labels = train_dataset.pop('MPG')
+test_labels = test_dataset.pop('MPG')
